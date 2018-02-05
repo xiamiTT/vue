8c946b395624cba92c0190302d0e030786ef5a6b
xiami <xiami@qq.com>
2018-02-05 16:52:34 +0800

    注释语句

---
+++ src/compoent/App.vue
@@ -0,0 +1,24 @@
+<template>
+                <div>
+
+                </div>
+</template>
+
+<script>
+                export default {
+                        data() {
+                                return {
+                                                title: 'Hello World!'
+                                }
+                }            
+                }
+</script>
+
+<style scoped>
+
+h1 {
+                font-size: 30px;
+                color: red;
+}
+
+</style>
